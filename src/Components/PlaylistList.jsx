cb232ea6bbc7e06d4424b37172f512174b4008e3
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:23:33 +0200

    Update playlist list component

--- src/Components/PlaylistList.jsx
+++ src/Components/PlaylistList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Playlist from './Playlist';
 
-const PlaylistList = function playlistList() {
+const PlaylistList = function playlistList(props) {
+  const {playlists} = props;
   return (
     <div className="col-lg-12 mb-3">
       <h3 className="text-success text-center mb-3">Playlists</h3>
@@ -11,15 +12,12 @@ const PlaylistList = function playlistList() {
             <th scope="col">#</th>
             <th scope="col">Name</th>
             <th scope="col">Owner</th>
+            <th scope="col">Handle</th>
             <th scope="col">Total Tracks</th>
           </tr>
         </thead>
         <tbody>
-          <Playlist />
-          <Playlist />
-          <Playlist />
-          <Playlist />
-          <Playlist />
+          { playlists.items.map((playlist) => <Playlist key={playlist.id} playlist={playlist} />) }
         </tbody>
       </table>
     </div>
