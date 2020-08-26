450edb70322c4032f864d29e9c198e913e690f33
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:24:38 +0200

    Update episode component

--- src/Components/Episode.jsx
+++ src/Components/Episode.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Episode = function episode() {
+const Episode = function episode(props) {
+  const {episode} = props;
   return (
     <tr>
       <td className="align-middle">1</td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Kiss from a rose</a>
+        <a className="btn btn-sm btn-success">{episode.name}</a>
       </td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Batman Forever</a>
+        <a className="btn btn-sm btn-success">Show Name Not Available</a>
       </td>
-      <td className="align-middle">03:10</td>
+      <td className="align-middle">{episode.duration_ms}</td>
       <td className="align-middle">
         <button type="button" className="btn btn-sm btn-success" data-toggle="modal" data-target="#modal">Add to Playlist</button>
       </td>
