88e2c7224f656041d14fe6ff43c1d4d02107334a
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:22:17 +0200

    Update track component

--- src/Components/Track.jsx
+++ src/Components/Track.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const Track = function track() {
+const Track = function track(props) {
+  const {track} = props;
   return (
     <tr>
       <td className="align-middle">1</td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Kiss from a rose</a>
+        <a className="btn btn-sm btn-success">{track.name}</a>
       </td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Batman Forever</a>
+        <a className="btn btn-sm btn-success">{track.album.name}</a>
       </td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Seal</a>
+        { track.album.artists.map((artist) => <a key={artist.id} className="btn btn-sm btn-success mr-2" href="track.html">{artist.name}</a>) }
       </td>
       <td className="align-middle">
         <button type="button" className="btn btn-sm btn-success" data-toggle="modal" data-target="#modal">Add to Playlist</button>
