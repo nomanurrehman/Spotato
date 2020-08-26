d82a8044a11877f88eac773c1ec6a7bf06652a33
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:26:24 +0200

    Update album component

--- src/Components/Album.jsx
+++ src/Components/Album.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Album = function album() {
+const Album = function album(props) {
+  const {album} = props;
+
+  function shortenName(name){
+    if(name.length > 50){
+      return name.substr(0, 50) + '...';
+    }
+    return name;
+  }
+
   return (
     <tr>
-      <td className="align-middle">1</td>
-      <td className="align-middle">
-        <a className="btn btn-sm btn-success" href="track.html">Batman Begins</a>
+      <td className="align-middle fit">1</td>
+      <td className="align-middle fit">
+        <a className="btn btn-sm btn-success" href="track.html">{shortenName(album.name)}</a>
       </td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success" href="track.html">James Newton Howard</a>
-        <a className="btn btn-sm btn-success" href="track.html">Hans Zimmer</a>
+        { album.artists.map((artist) => <a key={artist.id} className="btn btn-sm btn-success mr-2" href="track.html">{artist.name}</a>) }
       </td>
-      <td>12</td>
+      <td>{album.total_tracks}</td>
     </tr>
   );
 };
 
-Album.propTypes = {};
-Album.defaultProps = {};
+Album.propTypes = {
+  album: PropTypes.shape({
+    name: PropTypes.string,
+    artist: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string
+    })),
+    total_tracks: PropTypes.number,
+  }),
+};
+
+Album.defaultProps = {
+  album: {}
+};
 
 export default Album;
