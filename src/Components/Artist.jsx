b82a6eac911ed553e185416a3d9b34ed7e760e48
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:24:57 +0200

    Update artist component

--- src/Components/Artist.jsx
+++ src/Components/Artist.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Artist = function artist() {
+const Artist = function artist(props) {
+  const {artist} = props;
   return (
     <tr>
-      <td className="align-middle">1</td>
+      <td className="align-middle fit">1</td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Hans Zimmer</a>
+        <a className="btn btn-sm btn-success">{artist.name}</a>
       </td>
     </tr>
   );
 };
 
-Artist.propTypes = {};
-Artist.defaultProps = {};
+Artist.propTypes = {
+  artist: PropTypes.shape({
+    name: PropTypes.string
+  })
+};
+
+Artist.defaultProps = {
+  artist: {}
+};
 
 export default Artist;
