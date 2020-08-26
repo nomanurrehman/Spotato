8813db4d119793d5c0b317cb214d27a8d2ea1542
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-08-26 13:23:47 +0200

    Update playlist component

--- src/Components/Playlist.jsx
+++ src/Components/Playlist.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const Playlist = function playlist() {
+const Playlist = function playlist(props) {
+  const {playlist} = props;
   return (
     <tr>
       <td className="align-middle">1</td>
       <td className="align-middle">
-        <a className="btn btn-sm btn-success">Batman Soundtracks</a>
+        <a className="btn btn-sm btn-success">{playlist.name}</a>
       </td>
-      <td className="align-middle">Jonathan Luo</td>
-      <td className="align-middle">174</td>
+      <td className="align-middle">{playlist.owner.display_name}</td>
+      <td className="align-middle">{playlist.owner.id}</td>
+      <td className="align-middle">{playlist.tracks.total}</td>
     </tr>
   );
 };
